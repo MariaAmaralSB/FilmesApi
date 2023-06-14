b27f803edd6927b59eb383c0cf31352a063363f8
Maria Eduarda Amaral <44277668+MariaAmaralSB@users.noreply.github.com>
2023-06-13 22:35:43 -0300

    Adequação a padrões

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -6,34 +6,34 @@ namespace FilmesApi.Controllers
 
     [ApiController]
     [Route("[controller]")]
-    public class FilmeController : Controller
+    public class FilmeController:Controller
     {
         private static List<Filme> filmes = new List<Filme>();
         private static int id = 0;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            {
-                filme.Id = id++;
-                filmes.Add(filme);
-                Console.WriteLine(filme.Titulo);
-                Console.WriteLine(filme.Duracao);
-            }
-            
+            filme.Id=id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmePorId),
+                new {id = filme.Id},
+                filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip =0,
+        public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0,
         [FromQuery] int take = 50)
         {
             return filmes.Skip(skip).Take(take);
         }
 
         [HttpGet("{id}")]
-        public Filme? RecuperarFilmePorId(int id)
+        public IActionResult RecuperarFilmePorId(int id)
         {
-           return filmes.FirstOrDefault(filme => filme.Id == id);
+            var filme = filmes.FirstOrDefault(filme => filme.Id==id);
+            if(filme==null) return NotFound();
+            return Ok(filme);
         }
     }
 }
